1383f41c379b98e538918bb8b924340d95eb426a
openHacking <alexliu518@gmail.com>
2021-06-25 17:58:38 +0800

    first commit

---
+++ src/main/java/com/lwebapp/spring/postgresql/cruddemo/controller/TutorialController.java
@@ -0,0 +1,134 @@
+package com.lwebapp.spring.postgresql.cruddemo.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.http.annotation.CrossOrigin;
+import org.springframework.web.http.annotation.DeleteMapping;
+import org.springframework.web.http.annotation.GetMapping;
+import org.springframework.web.http.annotation.PathVariable;
+import org.springframework.web.http.annotation.PostMapping;
+import org.springframework.web.http.annotation.RequestBody;
+import org.springframework.web.http.annotation.RequestMapping;
+import org.springframework.web.http.annotation.RequestParam;
+import org.springframework.web.http.annotation.RestController;
+
+import com.lwebapp.spring.postgresql.cruddemo.model.Tutorial;
+import com.lwebapp.spring.postgresql.cruddemo.repository.TutorialRepository;
+
+@CrossOrigin(origins = "http://localhost:8081")
+@RestController
+@RequestMapping("/api")
+public class TutorialController {
+    @Autowired
+    TutorialRepository tutorialRepository;
+
+    @GetMapping("/tutorials")
+    public ResponseEntity<List<Tutorial>> getAllTutorials(@RequestParam(required = false) String title){
+
+        try {
+            List<Tutorial> tutorials = new ArrayList<Tutorial>();
+
+            if(title == null){
+                tutorialRepository.findAll().forEach(tutorials::add);
+            }else{
+                tutorialRepository.findByTitleContaining(title).forEach(tutorials::add);
+            }
+
+            if(tutorials.isEmpty()){
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+
+            return new ResponseEntity<>(tutorials,HttpStatus.OK);
+        } catch (Exception e) {
+            //TODO: handle exception
+            return new ResponseEntity<>(null,HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+    }
+
+    @GetMapping("/tutorials/{id}")
+    public ResponseEntity<Tutorial> getTutorialById(@PathVariable("id") long id) {
+        Optional<Tutorial> tutorialData = tutorialRepository.findById(id);
+
+        if(tutorialData.isPresent()){
+            return new ResponseEntity<>(tutorialData.get(),HttpStatus.OK);
+        }else{
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+    }
+
+    @PostMapping("tutorials")
+    public ResponseEntity<Tutorial> createTutorial(@RequestBody Tutorial tutorial){
+        try {
+            Tutorial _tutorial = tutorialRepository.save(new Tutorial(tutorial.getTitle(),tutorial.getDescription(),false));
+            return new ResponseEntity<>(_tutorial,HttpStatus.CREATED);
+        } catch (Exception e) {
+            //TODO: handle exception
+            return new ResponseEntity<>(null,HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+    }
+
+    @PutMapping("/tutorial/{id}")
+    public ResponseEntity<Tutorial> updateTutorial(@PathVariable("id") long id, @RequestBody Tutorial tutorial) {
+        Optional<Tutorial> tutorialData = tutorialRepository.findById(id);
+
+        if(tutorialData.isPresent()){
+            Tutorial _tutorial = tutorialData.get();
+            _tutorial.setTitle(tutorial.getTitle());
+            _tutorial.setDescription(tutorial.getDescription());
+            _tutorial.setPublished(tutorial.isPublished());
+            return new ResponseEntity<>(tutorialRepository.save(_tutorial),HttpStatus.OK);
+        }else{
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+    }
+
+    @DeleteMapping("/tutorials/{id}")
+    public ResponseEntity<HttpStatus> deleteTutorial(@PathVariable("id") long id) {
+        try {
+            tutorialRepository.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            //TODO: handle exception
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+    }
+
+    @DeleteMapping("/tutorials")
+    public ResponseEntity<HttpStatus> deleteAllTutorials(){
+        try {
+            tutorialRepository.deleteAll();
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            //TODO: handle exception
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+    }
+
+    @GetMapping("/tutorials/published")
+    public ResponseEntity<List<Tutorial>> findByPublished(){
+        try {
+            List<Tutorial> tutorials = tutorialRepository.findByPublished(true);
+
+            if(tutorials.isEmpty()){
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+
+            return new ResponseEntity<>(tutorials,HttpStatus.OK);
+        } catch (Exception e) {
+            //TODO: handle exception
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+    }
+}
---
+++ src/test/java/com/lwebapp/spring/postgresql/cruddemo/CrudDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lwebapp.spring.postgresql.cruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
