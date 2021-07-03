3de90700fb8944edb4a376ed57526dcc874c9949
openHacking <alexliu518@gmail.com>
2021-07-03 18:15:10 +0800

    add cors

--- src/main/java/com/lwebapp/spring/postgresql/cruddemo/CrudDemoApplication.java
+++ src/main/java/com/lwebapp/spring/postgresql/cruddemo/CrudDemoApplication.java
@@ -2,6 +2,9 @@ package com.lwebapp.spring.postgresql.cruddemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class CrudDemoApplication {
@@ -10,4 +13,20 @@ public class CrudDemoApplication {
 		SpringApplication.run(CrudDemoApplication.class, args);
 	}
 
+	// ref:https://spring.io/guides/gs/rest-service-cors/
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMapping(CorsRegistry registry) {
+				registry.addMapping("/api")
+				.allowedOrigins("http://localhost:3000")
+				.allowedMethods("PUT", "DELETE")
+				.allowedHeaders("")
+				.exposedHeaders("")
+				.allowCredentials(true)
+				.maxAge(3600);
+			}
+		};
+	}
 }
